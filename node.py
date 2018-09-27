72642ae4782e52cfa44ab6ac24773d5bc757eb5f
Jiashen Cao <caojiashen24@gmail.com>
2018-09-27 18:29:37 -0400

    [Fix] node input fix

--- node.py
+++ node.py
@@ -173,10 +173,10 @@ class Node:
         datatype = np.uint8 if req['type'] == 8 else np.float32
 
         shape = tuple([int(entry) for entry in req['shape'].split(' ')])
-        if self.input_shape is not None or shape != self.input_shape:
-            X = np.random.random_sample(self.input_shape)
-        else:
+        if self.input_shape is None or shape == self.input_shape:
             X = np.fromstring(bytestr, datatype).reshape(shape)
+        else:
+            X = np.random.random_sample(self.input_shape)
 
         self.input.enqueue(X)
         self.prepare_data += time.time() - start
